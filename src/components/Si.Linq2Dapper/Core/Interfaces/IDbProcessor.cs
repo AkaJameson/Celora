977029a68987cb0a569bb904615e5cc2e6ac49e8
辛京爵 <xinjingjue@ganweisoft.com>
2025-04-07 21:48:27 +0800

    feat:删除linqToDapper

--- src/components/Si.Linq2Dapper/Core/Interfaces/IDbProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Si.Linq2Dapper.Core.Interfaces
-{
-    internal interface IDbProcessor
-    {
-        string BuildLimitClause(int skip, int take);  
-        string ParameterPrefix { get; }               
-        string EscapeIdentifier(string name);         
-    }
-}
