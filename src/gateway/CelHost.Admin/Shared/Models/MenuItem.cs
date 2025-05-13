f02ebccf718b5b9a57664d5eeca7bf359d33295a
辛京爵 <xinjingjue@ganweisoft.com>
2025-05-13 10:41:01 +0800

    feat:更新

---
+++ src/gateway/CelHost.Admin/Shared/Models/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CelHost.Admin.Shared.Models
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public bool IsExpanded { get; set; } = false;
+        public List<SubMenuItem> SubItems { get; set; }
+    }
+    public class SubMenuItem
+    {
+        /// <summary>
+        /// 子菜单项名称
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+        private string url;
+        /// <summary>
+        /// 子菜单项导航链接
+        /// </summary>
+        public string Url { get { return url; } set { url = value.StartsWith("/") ? value : ($"/{value}"); } }
+    }
+}
