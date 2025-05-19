3340c8beb54cfda5242a383c3ecbf76b7219888c
辛京爵 <xinjingjue@ganweisoft.com>
2025-05-19 10:23:53 +0800

    feat:更新

--- src/gateway/CelHost.Admin/wwwroot/js/Script.js
+++ src/gateway/CelHost.Admin/wwwroot/js/Script.js
@@ -6,3 +6,32 @@ export function downloadFileFromBytes(filename, content) {
     link.click();
     URL.revokeObjectURL(link.href);
 }
+export function CanBeFullScreen() {
+    if (document.fullscreenEnabled || document.webketFullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled) {
+        return true;
+    }
+    return false;
+}
+export function FullScreen() {
+    const element = document.documentElement;
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    }
+}
+export function ExitFullScreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
